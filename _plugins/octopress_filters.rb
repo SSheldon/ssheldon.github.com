924a54b3c684178af32d20fdfdcd2fe61116aee6
Steven Sheldon <steven@sasheldon.com>
2014-05-11 00:28:10 -0700

    Simplified RSS XML and removed unnecessary filters.

--- _plugins/octopress_filters.rb
+++ _plugins/octopress_filters.rb
@@ -9,19 +9,6 @@ module OctopressLiquidFilters
     return (content.nil?) ? input : content
   end
 
-  # Escapes CDATA sections in post content
-  def cdata_escape(input)
-    input.gsub(/<!\[CDATA\[/, '&lt;![CDATA[').gsub(/\]\]>/, ']]&gt;')
-  end
-
-  # Replaces relative urls with full urls
-  def expand_urls(input, url='')
-    url ||= '/'
-    input.gsub /(\s+(href|src)\s*=\s*["|']{1})(\/[^\"'>]*)/ do
-      $1+url+$3
-    end
-  end
-
 end
 Liquid::Template.register_filter OctopressLiquidFilters
 
