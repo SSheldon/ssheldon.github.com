9197dadae5255aab577e10eefcd4a238a7b8e667
Steven Sheldon <steven@sasheldon.com>
2014-12-07 13:48:55 -0800

    Removed addCodeLineNumbers function from octopress.js.
    Since there were no gist-highlight divs,it didn't do anything.

--- javascripts/octopress.js
+++ javascripts/octopress.js
@@ -53,22 +53,6 @@ function testFeatures() {
   }
 }
 
-function addCodeLineNumbers() {
-  if (navigator.appName === 'Microsoft Internet Explorer') { return; }
-  $('div.gist-highlight').each(function(code) {
-    var tableStart = '<table><tbody><tr><td class="gutter">',
-        lineNumbers = '<pre class="line-numbers">',
-        tableMiddle = '</pre></td><td class="code">',
-        tableEnd = '</td></tr></tbody></table>',
-        count = $('.line', code).length;
-    for (var i=1;i<=count; i++) {
-      lineNumbers += '<span class="line-number">'+i+'</span>\n';
-    }
-    var table = tableStart + lineNumbers + tableMiddle + '<pre>'+$('pre', code).html()+'</pre>' + tableEnd;
-    $(code).html(table);
-  });
-}
-
 function flashVideoFallback(){
   var flashplayerlocation = "/assets/jwplayer/player.swf",
       flashplayerskin = "/assets/jwplayer/glow/glow.xml";
@@ -104,7 +88,6 @@ $('document').ready(function() {
   testFeatures();
   wrapFlashVideos();
   flashVideoFallback();
-  addCodeLineNumbers();
   getNav();
   addSidebarToggler();
 });
